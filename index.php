399fbfac40a2ffc52656a6adc736901e5bed3cbd
Udale-Ameh Ojimaojo <udaleamehoj@gmail.com>
2017-09-05 16:59:20 +0100

    changing root directory

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     //This is official boiler plate for the workabee app
 	include 'includes/inc.objects.php';
-	include './indexController.php';
+	include '/app/indexController.php';
     
 ?>
